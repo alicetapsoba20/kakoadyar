5df5a6cea1f480bdbbedf9d814ba698fb5659941
alicetapsoba20 <alicetapsoba20@gmail.comgit config --global user.email alicetapsoba20@gmail.comgit config --global user.email alicetapsoba20@gmail.com>
2022-10-28 14:58:19 +0000

    modification

--- kakaodayar/settings.py
+++ kakaodayar/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "kakaodayar/static")
 ]
