a6ee63782dfe7a58417429bb1d88bf78db06c2a0
alicetapsoba20 <alicetapsoba20@gmail.comgit config --global user.email alicetapsoba20@gmail.comgit config --global user.email alicetapsoba20@gmail.com>
2022-10-28 15:48:24 +0000

    modification

--- kakaodayar/settings.py
+++ kakaodayar/settings.py
@@ -72,23 +72,23 @@ WSGI_APPLICATION = 'kakaodayar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
+
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'data_kakoadyar',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Alice@2022',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'data_kakoadyar',
-        'USER': 'postgres',
-        'PASSWORD': 'Alice@2022',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
 
 
 # Password validation
